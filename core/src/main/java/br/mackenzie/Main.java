4c977ccf3b97dba1da39c3ba11e8b93601753857
lucasarenda <lucas.arenda@gmail.com>
2026-05-06 18:14:56 +0000

    botão game over e botão restart

--- core/src/main/java/br/mackenzie/Main.java
+++ core/src/main/java/br/mackenzie/Main.java
@@ -2,6 +2,7 @@ package br.mackenzie;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,6 +12,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Main implements ApplicationListener {
 
@@ -25,6 +27,14 @@ public class Main implements ApplicationListener {
     Texture trilhos2Texture;
     Texture obstacleTexture;
 
+    Texture gameoverScreen;
+
+    Texture restartBtn;
+    Texture restartBtnHover;
+    Rectangle restartRect;
+
+    boolean gameOver = false;
+
     Sprite heroSprite;
 
     SpriteBatch spriteBatch;
@@ -33,7 +43,6 @@ public class Main implements ApplicationListener {
     float speed = 4f;
     float groundY = 1.5f;
 
-    // PULO
     float velocityY = 0;
     float gravity = -20f;
     float jumpForce = 8f;
@@ -45,7 +54,8 @@ public class Main implements ApplicationListener {
     Rectangle heroRect;
     Rectangle obstacleRect;
 
-    // Parallax
+    Vector3 touchPos = new Vector3();
+
     float offsetCeu = 0;
     float offsetPredios1 = 0;
     float offsetSombra = 0;
@@ -66,6 +76,11 @@ public class Main implements ApplicationListener {
         trilhos2Texture = new Texture("trilhos6.png");
         obstacleTexture = new Texture("obstaculo.png");
 
+        gameoverScreen = new Texture("gameoverScreen.png");
+
+        restartBtn = new Texture("restart.png");
+        restartBtnHover = new Texture("restartHover.png");
+
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
 
@@ -77,11 +92,13 @@ public class Main implements ApplicationListener {
 
         heroRect = new Rectangle();
         obstacleRect = new Rectangle();
+
+        
+        restartRect = new Rectangle(3, 0.5f, 2, 0.8f);
     }
 
     @Override
     public void resize(int width, int height) {
-        if(width <= 0 || height <= 0) return;
         viewport.update(width, height, true);
     }
 
@@ -93,19 +110,30 @@ public class Main implements ApplicationListener {
     }
 
     private void input() {
+
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+        viewport.unproject(touchPos);
+
+        if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            if(gameOver && restartRect.contains(touchPos.x, touchPos.y)) {
+                resetGame();
+            }
+        }
+
+        if(gameOver) return;
+
         float delta = Gdx.graphics.getDeltaTime();
 
-        if(Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.RIGHT)) {
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             heroSprite.translateX(speed * delta);
         }
 
-        if(Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.LEFT)) {
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             heroSprite.translateX(-speed * delta);
         }
 
-        //  PULO
-        if((Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.SPACE) ||
-            Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.UP))
+        if((Gdx.input.isKeyJustPressed(Input.Keys.SPACE) ||
+            Gdx.input.isKeyJustPressed(Input.Keys.UP))
             && !isJumping) {
 
             velocityY = jumpForce;
@@ -115,98 +143,70 @@ public class Main implements ApplicationListener {
 
     private void logic() {
         float delta = Gdx.graphics.getDeltaTime();
-        float worldWidth = viewport.getWorldWidth();
 
-        // Gravidade
-        velocityY += gravity * delta;
-        heroSprite.translateY(velocityY * delta);
+        if(!gameOver) {
 
-        if(heroSprite.getY() <= groundY) {
-            heroSprite.setY(groundY);
-            velocityY = 0;
-            isJumping = false;
+            float worldWidth = viewport.getWorldWidth();
+
+            velocityY += gravity * delta;
+            heroSprite.translateY(velocityY * delta);
+
+            if(heroSprite.getY() <= groundY) {
+                heroSprite.setY(groundY);
+                velocityY = 0;
+                isJumping = false;
+            }
+
+            heroSprite.setTexture(isJumping ? heroJumpTexture : heroTexture);
+
+            heroSprite.setX(MathUtils.clamp(
+                heroSprite.getX(),
+                0,
+                worldWidth - heroSprite.getWidth()
+            ));
+
+            obstacleTimer += delta;
+            if(obstacleTimer > 1f) {
+                obstacleTimer = 0;
+                Sprite obstacle = new Sprite(obstacleTexture);
+                obstacle.setSize(1, 1);
+                obstacle.setPosition(worldWidth, 1.3f);
+                obstacles.add(obstacle);
+            }
+
+            for(int i = obstacles.size - 1; i >= 0; i--) {
+                Sprite obstacle = obstacles.get(i);
+
+                obstacle.translateX(-3f * delta);
+
+                if(obstacle.getX() < -1) {
+                    obstacles.removeIndex(i);
+                    continue;
+                }
+
+                heroRect.set(heroSprite.getX(), heroSprite.getY(), 0.7f, 0.7f);
+                obstacleRect.set(obstacle.getX(), obstacle.getY(), 0.7f, 0.7f);
+
+                if(heroRect.overlaps(obstacleRect)) {
+                    gameOver = true;
+                }
+            }
         }
 
-        // Troca sprite
-        if(isJumping) {
-            heroSprite.setTexture(heroJumpTexture);
-        } else {
-            heroSprite.setTexture(heroTexture);
-        }
-
-        // Limite horizontal
-        heroSprite.setX(MathUtils.clamp(
-            heroSprite.getX(),
-            0,
-            worldWidth - heroSprite.getWidth()
-        ));
-
-        // Parallax
         offsetCeu -= 0.1f * delta;
         offsetPredios1 -= 0.3f * delta;
         offsetSombra -= 0.4f * delta;
         offsetPredios2 -= 0.6f * delta;
         offsetTrilhos1 -= 1.5f * delta;
-
-        // Spawn obstáculos
-        obstacleTimer += delta;
-        if(obstacleTimer > 1f) {
-            obstacleTimer = 0;
-
-            Sprite obstacle = new Sprite(obstacleTexture);
-            obstacle.setSize(1, 1);
-            obstacle.setPosition(worldWidth, 1.3f);
-
-            obstacles.add(obstacle);
-        }
-
-        for(int i = obstacles.size - 1; i >= 0; i--) {
-            Sprite obstacle = obstacles.get(i);
-
-            obstacle.translateX(-3f * delta);
-
-            if(obstacle.getX() < -obstacle.getWidth()) {
-                obstacles.removeIndex(i);
-                continue;
-            }
-
-            
-            float heroWidth = heroSprite.getWidth() * 0.7f;
-            float heroHeight = heroSprite.getHeight() * 0.7f;
-
-            heroRect.set(
-                heroSprite.getX() + (heroSprite.getWidth() - heroWidth) / 2,
-                heroSprite.getY() + (heroSprite.getHeight() - heroHeight) / 2,
-                heroWidth,
-                heroHeight
-            );
-
-            
-            float obsWidth = obstacle.getWidth() * 0.7f;
-            float obsHeight = obstacle.getHeight() * 0.7f;
-
-            obstacleRect.set(
-                obstacle.getX() + (obstacle.getWidth() - obsWidth) / 2,
-                obstacle.getY() + (obstacle.getHeight() - obsHeight) / 2,
-                obsWidth,
-                obsHeight
-            );
-
-            if(heroRect.overlaps(obstacleRect)) {
-                System.out.println("COLIDIU!");
-                obstacles.removeIndex(i);
-            }
-        }
     }
 
     private void drawLayer(Texture texture, float offset) {
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
+        float w = viewport.getWorldWidth();
+        float h = viewport.getWorldHeight();
+        float x = offset % w;
 
-        float x = offset % worldWidth;
-
-        spriteBatch.draw(texture, x, 0, worldWidth, worldHeight);
-        spriteBatch.draw(texture, x + worldWidth, 0, worldWidth, worldHeight);
+        spriteBatch.draw(texture, x, 0, w, h);
+        spriteBatch.draw(texture, x + w, 0, w, h);
     }
 
     private void draw() {
@@ -214,7 +214,6 @@ public class Main implements ApplicationListener {
 
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-
         spriteBatch.begin();
 
         drawLayer(ceuTexture, offsetCeu);
@@ -224,13 +223,39 @@ public class Main implements ApplicationListener {
         drawLayer(trilhos1Texture, offsetTrilhos1);
         drawLayer(trilhos2Texture, offsetTrilhos2);
 
-        for(Sprite obstacle : obstacles) {
-            obstacle.draw(spriteBatch);
-        }
-
+        for(Sprite obstacle : obstacles) obstacle.draw(spriteBatch);
         heroSprite.draw(spriteBatch);
 
+        if(gameOver) {
+            float w = viewport.getWorldWidth();
+            float h = viewport.getWorldHeight();
+
+            
+            float goWidth = w * 1.0f;
+            float goHeight = h * 1.0f;
+            float goX = (w - goWidth) / 2;
+            float goY = h * 0.2f;
+
+            spriteBatch.draw(gameoverScreen, goX, goY, goWidth, goHeight);
+
+            boolean hover = restartRect.contains(touchPos.x, touchPos.y);
+
+            spriteBatch.draw(
+                hover ? restartBtnHover : restartBtn,
+                restartRect.x,
+                restartRect.y,
+                restartRect.width,
+                restartRect.height
+            );
+        }
+
         spriteBatch.end();
+    }
+
+    private void resetGame() {
+        gameOver = false;
+        obstacles.clear();
+        heroSprite.setPosition(1, groundY);
     }
 
     @Override public void pause() {}
@@ -247,6 +272,9 @@ public class Main implements ApplicationListener {
         trilhos1Texture.dispose();
         trilhos2Texture.dispose();
         obstacleTexture.dispose();
+        gameoverScreen.dispose();
+        restartBtn.dispose();
+        restartBtnHover.dispose();
         spriteBatch.dispose();
     }
 }
